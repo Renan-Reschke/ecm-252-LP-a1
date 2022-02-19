e18c24ef9a71b311f0a9e8539a5e831c36abbe56
Renan Reschke <renanreschke@hotmail.com>
2022-02-19 12:25:54 -0300

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+// //Constante
+// const nome = "José"
+// const idade = 27
+// //
+
+// //Variável
+// let a = 2 //escopo local
+// a = "abc" //Tipagem dinâmica
+// //
+
+// //
+// var c = 2 + 3 //escopo global
+// c = 5
+// //
+
+// var ling = "Javascript"
+// console.log("Aprendendo " + ling)
+// //variável pode ser redeclarada
+// var ling = "Java"
+// console.log("Aprendendo " + ling)
+
+var idade = 18
+console.log("Oi, " + nome)
+if(idade > 18 || idade > 2){
+    var nome = "João"
+    console.log("Parabéns, " + nome)
+    console.log("Você pode dirigir")
+}
+console.log("Até mais, " + nome)
